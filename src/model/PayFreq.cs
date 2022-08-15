57358244c30cc647c622e2bec2374ce7c40f8511
jonathan souter <uuupah@gmail.com>
2022-08-15 22:27:34 +0930

    add currency formatting tax deductions in iohelper

--- src/model/PayFreq.cs
+++ src/model/PayFreq.cs
@@ -5,9 +5,6 @@ namespace VeritechChallenge.src.model
     /// </summary>
     public enum PayFreq
     {
-        // NOTE this could benefit from some more user friendly enum names as below:
-        //      Weekly = 'W', Fortnightly = 'F', Monthly = 'M'
-        //      this would unfortunately require a bit of fiddling so i've prioritised other tasks for the time being
         W = 'W', F = 'F', M = 'M'
     }
 }
